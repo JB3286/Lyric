90449bbbb56f4edece5871dc49fe49647f2b96ed
JB3286 <58040211+JB3286@users.noreply.github.com>
2024-01-17 12:24:30 +0100

    Added some font-familys to the templates font

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FooterComponent } from './components/footer/footer/footer.component';
 import { LandingPageComponent } from './components/landingPage/landingPage.component';
+import { NavigationComponent } from './components/navigation/navigation/navigation.component';
 
 const routes: Routes = [
   {path:'', pathMatch:'full', component: LandingPageComponent},
-  {path:'hello', component: FooterComponent}
+  {path:'hello', component: NavigationComponent}
 ];
 
 @NgModule({

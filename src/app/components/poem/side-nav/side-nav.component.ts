6898865254429fa625f0af0b70f3559b3dbc0ce2
JB3286 <58040211+JB3286@users.noreply.github.com>
2024-01-18 19:41:53 +0100

    Created the side-navigation for poem-component

---
+++ src/app/components/poem/side-nav/side-nav.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'side-nav',
+  templateUrl: './side-nav.component.html',
+  styleUrl: './side-nav.component.scss'
+})
+export class SideNavComponent {
+
+}

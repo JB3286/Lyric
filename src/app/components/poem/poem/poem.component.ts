5a1946bbcd8bc86edd2e61768b3dca2127d3b560
JB3286 <58040211+JB3286@users.noreply.github.com>
2024-01-19 03:23:27 +0100

    Developed a Poems-Content-Table for splitting the text in poetry

--- src/app/components/poem/poem/poem.component.ts
+++ src/app/components/poem/poem/poem.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { Poem } from '../../../types/poem';
+import { OnInit } from '@angular/core';
 
 @Component({
   selector: 'poem',
@@ -8,13 +10,35 @@ import { Router } from '@angular/router';
 })
 export class PoemComponent {
 
-    constructor(private router:Router){}
+  poem: Poem = {} as Poem;
+  poems: Poem[] = [] as Poem[];
+
+  strings: string[] = [
+    "2 Marder werden 20 Rabenkrähen fressen",
+    "20 Rabenkrähem werden",
+    "30 Mütter werden gefickt",
+    "",
+    "",
+    "Fick deinen Vater",
+  ];
+
+  getArrayAccess(): string[]{
+    return this.poem.content;
+  }
+
+
+    constructor(private router:Router){
+      this.poem = { id:"0", date:"17.01.2024",title:"2024", category:"Philsophisches",content:["2 Marder werden 20 Rabenkrähen fressen","20 Rabenkrähem werden","30 Mütter werden grifkt","","","Fick deinen Vater"] ,comments:"(01.01.20, 01.01.21, 10.10.23: Umstellung Verse der vorletzten Strophe, Wechsel letzte/vorletzte Strophe, 06.01.24: Jahresaktualisierung)"}
+    }
 
     onInit(): void{
-      window.scrollTo(0,10000);
+      console.log(this.poem.content);
     }
 
     home(){
       this.router.navigateByUrl('/');
     }
-}
+
+
+
+  }
